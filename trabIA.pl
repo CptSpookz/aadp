f274792b53d79b76ea7de0f2d68466855f9f4afe
GuilhermeOli97 <49626376+GuilhermeOli97@users.noreply.github.com>
2019-05-11 17:12:39 -0300

    Add files via upload

--- trabIA.pl
+++ trabIA.pl
@@ -11,8 +11,8 @@ sujeira([7,8]).
 elevador(2).
 elevador(8).
 
-S(X, Y) :- mover_direita(X,Y); mover_cima(X,Y); recolher_sujeira(X,Y,Z).
+S(X, Y) :- mover_direita(X,Y); mover_cima(X,Y,Cenario); recolher_sujeira(X,Y,Cenario).
 
 mover_direita([X,Y|Cauda],[X1,Y|Cauda]) :- Y1 is Y + 1, pertence (Y1, [0,1,2,3,4,5,6,7,8,9]).
-mover_cima([X,Y|Cauda],[X,Y1|Cauda]) :- elevador(Y),X1 is X + 1, pertence(X1,[0,1,2,3,4]).
+mover_cima([X,Y|Cauda],[X,Y1|Cauda],[_|[Elevador]]) :- pertence(Y,Elevador),X1 is X + 1, pertence(X1,[0,1,2,3,4]).
 recolher_sujeira([Posicao|[Z|_]],[Posicao|[Z1]],[Cabeca|_]) :- retirar_elemento(Posicao,Cabeca), Z1 is Z + 1, Z < 2.
